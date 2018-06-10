68b1ad6ac6070f760844ade2368ffaae510e98ab
gjvera <git@gabrieljosephvera.com>
2018-06-10 12:14:46 -0700

    Fixing problem 7

--- clojure/problem7/src/problem7/core.clj
+++ clojure/problem7/src/problem7/core.clj
@@ -2,13 +2,14 @@
 
 (defn is-prime? 
   [x]
+  (if (= x 0) false
   (loop [iter 2 top (int (Math/sqrt x))]
     (if (> iter top)
       true
       (if (= 0 (mod x iter))
         false
-(recur (inc iter) top)))))
+(recur (inc iter) top))))))
 
 (defn -main 
   []
-  (println (nth (filter is-prime? (range)) 10002)))
+  (println (nth (filter is-prime? (range)) 10001)))
